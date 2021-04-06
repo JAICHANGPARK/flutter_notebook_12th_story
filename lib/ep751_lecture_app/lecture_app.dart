45785e79c902debce0c930729e457ed3c4e41e84
Dreamwalker <aristojeff@gmail.com>
2021-04-06 19:37:36 +0900

    Update lecture_app.dart

--- lib/ep751_lecture_app/lecture_app.dart
+++ lib/ep751_lecture_app/lecture_app.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class LectureApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/" : (context) => LectureHomePage()
+      },
+    );
   }
 }
