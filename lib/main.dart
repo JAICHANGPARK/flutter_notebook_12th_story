adf7bf1781dccec9aaf6015fdca57a37f40a87d6
Dreamwalker <aristojeff@gmail.com>
2021-04-06 19:34:07 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_12th_story/ep751_lecture_app/lecture_app.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(LectureApp());
 }
