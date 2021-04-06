608b5cf4e21709866e8a941fec61d0b87b31ee99
Dreamwalker <aristojeff@gmail.com>
2021-04-06 19:54:48 +0900

    Update lecture_home_page.dart

--- lib/ep751_lecture_app/lecture_home_page.dart
+++ lib/ep751_lecture_app/lecture_home_page.dart
@@ -12,35 +12,40 @@ class _LectureHomePageState extends State<LectureHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16)
-            )
-          ),
-          height: 72,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                iconSize: 28,
-                onPressed: (){
-                setState(() {
-                  bottomTab = 0;
-                });
-              }, icon: Icon(LineIcons.clock),
-              color: bottomTab == 0 ? Colors.blue : Colors.grey,),
-              IconButton(  color: bottomTab == 1 ? Colors.blue : Colors.grey,iconSize: 28,onPressed: (){setState(() {
-                bottomTab = 1;
-              });}, icon: Icon(LineIcons.comment)),
-              IconButton( color: bottomTab == 2 ? Colors.blue : Colors.grey, iconSize: 28,onPressed: (){setState(() {
-                bottomTab = 2;
-              });}, icon: Icon(LineIcons.stream)),
-              IconButton(  color: bottomTab == 3 ? Colors.blue : Colors.grey,iconSize: 28,onPressed: (){setState(() {
-                bottomTab = 3;
-              });}, icon: Icon(LineIcons.cog)),
-            ],
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
+        child: BottomAppBar(
+          elevation: 4,
+          child: Container(
+            decoration: BoxDecoration(
+
+            ),
+            height: 72,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  iconSize: 28,
+                  onPressed: (){
+                  setState(() {
+                    bottomTab = 0;
+                  });
+                }, icon: Icon(LineIcons.clock),
+                color: bottomTab == 0 ? Colors.blue : Colors.grey,),
+                IconButton(  color: bottomTab == 1 ? Colors.blue : Colors.grey,iconSize: 28,onPressed: (){setState(() {
+                  bottomTab = 1;
+                });}, icon: Icon(LineIcons.comment)),
+                IconButton( color: bottomTab == 2 ? Colors.blue : Colors.grey, iconSize: 28,onPressed: (){setState(() {
+                  bottomTab = 2;
+                });}, icon: Icon(LineIcons.stream)),
+                IconButton(  color: bottomTab == 3 ? Colors.blue : Colors.grey,iconSize: 28,onPressed: (){setState(() {
+                  bottomTab = 3;
+                });}, icon: Icon(LineIcons.cog)),
+              ],
+            ),
           ),
         ),
       ),
