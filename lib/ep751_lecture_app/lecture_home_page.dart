8cd62f5d437824f476d83d2adb73cd950c5eedd1
Dreamwalker <aristojeff@gmail.com>
2021-04-06 19:52:57 +0900

    Update lecture_home_page.dart

--- lib/ep751_lecture_app/lecture_home_page.dart
+++ lib/ep751_lecture_app/lecture_home_page.dart
@@ -14,6 +14,11 @@ class _LectureHomePageState extends State<LectureHomePage> {
     return Scaffold(
       bottomNavigationBar: BottomAppBar(
         child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16)
+            )
+          ),
           height: 72,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -26,9 +31,15 @@ class _LectureHomePageState extends State<LectureHomePage> {
                 });
               }, icon: Icon(LineIcons.clock),
               color: bottomTab == 0 ? Colors.blue : Colors.grey,),
-              IconButton(onPressed: (){}, icon: Icon(LineIcons.comment)),
-              IconButton(onPressed: (){}, icon: Icon(LineIcons.stream)),
-              IconButton(onPressed: (){}, icon: Icon(LineIcons.cog)),
+              IconButton(  color: bottomTab == 1 ? Colors.blue : Colors.grey,iconSize: 28,onPressed: (){setState(() {
+                bottomTab = 1;
+              });}, icon: Icon(LineIcons.comment)),
+              IconButton( color: bottomTab == 2 ? Colors.blue : Colors.grey, iconSize: 28,onPressed: (){setState(() {
+                bottomTab = 2;
+              });}, icon: Icon(LineIcons.stream)),
+              IconButton(  color: bottomTab == 3 ? Colors.blue : Colors.grey,iconSize: 28,onPressed: (){setState(() {
+                bottomTab = 3;
+              });}, icon: Icon(LineIcons.cog)),
             ],
           ),
         ),
