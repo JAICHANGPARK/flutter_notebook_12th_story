43d207ce0e7f461f479a2e6b45c4229820f8babf
Dreamwalker <aristojeff@gmail.com>
2021-04-07 21:06:18 +0900

    Update lecture_home_page.dart

--- lib/ep751_lecture_app/lecture_home_page.dart
+++ lib/ep751_lecture_app/lecture_home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 
-
 class LectureHomePage extends StatefulWidget {
   @override
   _LectureHomePageState createState() => _LectureHomePageState();
@@ -9,6 +8,7 @@ class LectureHomePage extends StatefulWidget {
 
 class _LectureHomePageState extends State<LectureHomePage> {
   int bottomTab = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,54 +20,73 @@ class _LectureHomePageState extends State<LectureHomePage> {
               right: 0,
               top: 0,
               child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Colors.white,
-                Colors.blue[50]!,
-                Colors.blue[100]!,
-
-
-              ])
-            ),
-
-          )),
-          Positioned(child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(24),
-                topRight: Radius.circular(24),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [
+                  Colors.white,
+                  Colors.blue[50]!,
+                  Colors.blue[100]!,
+                ])),
+              )),
+          Positioned(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  topRight: Radius.circular(24),
+                ),
+              ),
+              height: 72,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    iconSize: 28,
+                    onPressed: () {
+                      setState(() {
+                        bottomTab = 0;
+                      });
+                    },
+                    icon: Icon(LineIcons.clock),
+                    color: bottomTab == 0 ? Colors.blue : Colors.grey,
+                  ),
+                  IconButton(
+                      color: bottomTab == 1 ? Colors.blue : Colors.grey,
+                      iconSize: 28,
+                      onPressed: () {
+                        setState(() {
+                          bottomTab = 1;
+                        });
+                      },
+                      icon: Icon(LineIcons.comment)),
+                  IconButton(
+                      color: bottomTab == 2 ? Colors.blue : Colors.grey,
+                      iconSize: 28,
+                      onPressed: () {
+                        setState(() {
+                          bottomTab = 2;
+                        });
+                      },
+                      icon: Icon(LineIcons.stream)),
+                  IconButton(
+                      color: bottomTab == 3 ? Colors.blue : Colors.grey,
+                      iconSize: 28,
+                      onPressed: () {
+                        setState(() {
+                          bottomTab = 3;
+                        });
+                      },
+                      icon: Icon(LineIcons.cog)),
+                ],
               ),
             ),
-            height: 72,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  iconSize: 28,
-                  onPressed: (){
-                    setState(() {
-                      bottomTab = 0;
-                    });
-                  }, icon: Icon(LineIcons.clock),
-                  color: bottomTab == 0 ? Colors.blue : Colors.grey,),
-                IconButton(  color: bottomTab == 1 ? Colors.blue : Colors.grey,iconSize: 28,onPressed: (){setState(() {
-                  bottomTab = 1;
-                });}, icon: Icon(LineIcons.comment)),
-                IconButton( color: bottomTab == 2 ? Colors.blue : Colors.grey, iconSize: 28,onPressed: (){setState(() {
-                  bottomTab = 2;
-                });}, icon: Icon(LineIcons.stream)),
-                IconButton(  color: bottomTab == 3 ? Colors.blue : Colors.grey,iconSize: 28,onPressed: (){setState(() {
-                  bottomTab = 3;
-                });}, icon: Icon(LineIcons.cog)),
-              ],
-            ),
+            bottom: 0,
+            left: 0,
+            right: 0,
           ),
-          bottom: 0,
-          left: 0, right: 0,)
+          Positioned(child: Column(children: [
+            
+          ],))
         ],
       ),
     );
