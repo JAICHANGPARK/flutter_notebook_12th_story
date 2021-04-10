143de4f704168afd5b53f97989cc8694e90f0c2f
Dreamwalker <aristojeff@gmail.com>
2021-04-10 18:00:41 +0900

    Create comic_home_page.dart

---
+++ lib/ep755_comic_app/ui/comic_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ComicHomePage extends StatefulWidget {
+  ComicHomePage({Key key}) : super(key: key);
+
+  @override
+  _ComicHomePageState createState() => _ComicHomePageState();
+}
+
+class _ComicHomePageState extends State<ComicHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
