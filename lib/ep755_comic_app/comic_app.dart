d17a81b7a61ae0f1808711856924fe3a79c4796d
Dreamwalker <aristojeff@gmail.com>
2021-04-10 17:59:43 +0900

    Update comic_app.dart

---
+++ lib/ep755_comic_app/comic_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ComicApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
