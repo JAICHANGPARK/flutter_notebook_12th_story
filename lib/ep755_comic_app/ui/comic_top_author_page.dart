d7709a3700efeb444696c2cc7f97e16b495df7e3
Dreamwalker <aristojeff@gmail.com>
2021-04-10 18:13:14 +0900

    Create comic_top_author_page.dart

---
+++ lib/ep755_comic_app/ui/comic_top_author_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ComicTopAuthorPage extends StatefulWidget {
+  ComicTopAuthorPage({Key key}) : super(key: key);
+
+  @override
+  _ComicTopAuthorPageState createState() => _ComicTopAuthorPageState();
+}
+
+class _ComicTopAuthorPageState extends State<ComicTopAuthorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
