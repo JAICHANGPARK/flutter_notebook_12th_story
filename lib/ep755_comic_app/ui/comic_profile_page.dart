86b2ad79ddd3bd454eca182562b77ed360715e7c
Dreamwalker <aristojeff@gmail.com>
2021-04-11 18:51:21 +0900

    Create comic_profile_page.dart

---
+++ lib/ep755_comic_app/ui/comic_profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ComicProfilePage extends StatefulWidget {
+  ComicProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _ComicProfilePageState createState() => _ComicProfilePageState();
+}
+
+class _ComicProfilePageState extends State<ComicProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
