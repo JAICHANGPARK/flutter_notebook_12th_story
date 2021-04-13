3075c0010a17aa1d4f48dc1dc4f61340634e3d58
Dreamwalker <aristojeff@gmail.com>
2021-04-13 20:56:56 +0900

    Create comic_trending_page.dart

---
+++ lib/ep755_comic_app/ui/comic_trending_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class ComicTrendingPage extends StatefulWidget {
+  @override
+  _ComicTrendingPageState createState() => _ComicTrendingPageState();
+}
+
+class _ComicTrendingPageState extends State<ComicTrendingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
