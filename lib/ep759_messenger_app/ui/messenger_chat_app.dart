12b282cd7f6ab37577648f1634e325611f4f05f0
Dreamwalker <aristojeff@gmail.com>
2021-04-14 21:01:22 +0900

    Create messenger_chat_app.dart

---
+++ lib/ep759_messenger_app/ui/messenger_chat_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MessengerChatPage extends StatefulWidget {
+  MessengerChatApp({Key key}) : super(key: key);
+
+  @override
+  _MessengerChatAppState createState() => _MessengerChatAppState();
+}
+
+class _MessengerChatAppState extends State<MessengerChatApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
