ffadcc53542a469c2a667ff3d51ab4a2d0e1c5b7
Dreamwalker <aristojeff@gmail.com>
2021-04-14 21:08:34 +0900

    Update messenger_chat_app.dart

--- lib/ep759_messenger_app/ui/messenger_chat_app.dart
+++ lib/ep759_messenger_app/ui/messenger_chat_app.dart
@@ -31,7 +31,12 @@ class _MessengerChatAppState extends State<MessengerChatPage> {
                   child: Row(
                     children: [
                       IconButton(onPressed: (){}, icon: Icon(Icons.attach_file_outlined)),
-                      Expanded(child: TextField()),
+                      Expanded(child: TextField(
+                        controller: _textEditingController,
+                        decoration: InputDecoration(
+                          hintText: "Write a message"
+                        ),
+                      )),
                       IconButton(onPressed: (){}, icon: Icon(Icons.sticky_note_2)),
                       IconButton(onPressed: (){}, icon: Icon(Icons.mic_none_outlined))
                     ],
