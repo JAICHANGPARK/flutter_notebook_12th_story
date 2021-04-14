01af1974fff267a2eefab0353e4ac6b4072e2e77
Dreamwalker <aristojeff@gmail.com>
2021-04-14 21:00:32 +0900

    Update messenger_app.dart

--- lib/ep759_messenger_app/messenger_app.dart
+++ lib/ep759_messenger_app/messenger_app.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class MessengerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp();
   }
 }
