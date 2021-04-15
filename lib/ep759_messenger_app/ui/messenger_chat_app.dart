8461237a478f31911e7bcc37dabc8d955a4240e4
Dreamwalker <aristojeff@gmail.com>
2021-04-15 20:29:31 +0900

    Update messenger_chat_app.dart

--- lib/ep759_messenger_app/ui/messenger_chat_app.dart
+++ lib/ep759_messenger_app/ui/messenger_chat_app.dart
@@ -138,7 +138,10 @@ class _MessengerChatAppState extends State<MessengerChatPage> {
                                                             ),
                                                           ),
                                                           Text(
-                                                              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat"),
+                                                              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat",
+                                                          style: TextStyle(
+                                                            color: Colors.grey[400]
+                                                          ),),
                                                           Padding(
                                                             padding: const EdgeInsets.symmetric(vertical: 4),
                                                             child: Image.network(
