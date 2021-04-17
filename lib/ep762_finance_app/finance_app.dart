040244392685cc7f5ea5d455afd828245109ed9f
Dreamwalker <aristojeff@gmail.com>
2021-04-17 20:43:01 +0900

    Update finance_app.dart

--- lib/ep762_finance_app/finance_app.dart
+++ lib/ep762_finance_app/finance_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'ui/finance_main_page.dart';
+
 class FinanceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
