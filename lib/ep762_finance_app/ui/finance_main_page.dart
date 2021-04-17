909b2785291bd20eaaadb3452a2dd8c90025c207
Dreamwalker <aristojeff@gmail.com>
2021-04-17 20:45:50 +0900

    Update finance_main_page.dart

--- lib/ep762_finance_app/ui/finance_main_page.dart
+++ lib/ep762_finance_app/ui/finance_main_page.dart
@@ -8,6 +8,17 @@ class FinanceMainPage extends StatefulWidget {
 class _FinanceMainPageState extends State<FinanceMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              
+            ],
+          ),
+        ),
+      ),
+
+    );
   }
 }
