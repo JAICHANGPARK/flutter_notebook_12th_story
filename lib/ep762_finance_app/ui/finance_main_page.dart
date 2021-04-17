affd6e46098d751b2bfafdbaa81ca6594813c215
Dreamwalker <aristojeff@gmail.com>
2021-04-17 20:42:57 +0900

    Create finance_main_page.dart

---
+++ lib/ep762_finance_app/ui/finance_main_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FinanceMainPage extends StatefulWidget {
+  @override
+  _FinanceMainPageState createState() => _FinanceMainPageState();
+}
+
+class _FinanceMainPageState extends State<FinanceMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
