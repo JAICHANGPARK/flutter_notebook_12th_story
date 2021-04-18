a5acbb898a5d61b57f633c22a1a2552f8eeee06d
Dreamwalker <aristojeff@gmail.com>
2021-04-18 18:00:03 +0900

    Update finance_main_page.dart

--- lib/ep762_finance_app/ui/finance_main_page.dart
+++ lib/ep762_finance_app/ui/finance_main_page.dart
@@ -153,26 +153,28 @@ class _FinanceMainPageState extends State<FinanceMainPage> {
                                 )),
                             Positioned(
                               child: Container(
-                                color: Colors.red,
+                                height: double.infinity,
+                                width: double.infinity,
                                 child: BezierChart(
-                                  bezierChartScale: BezierChartScale.CUSTOM, series: [
-                                    BezierLine(
-                                      data: [
-                                        DataPoint<double>(value: 10, xAxis: 0),
-                                        DataPoint<double>(value: 130, xAxis: 5),
-                                        DataPoint<double>(value: 50, xAxis: 10),
-                                        DataPoint<double>(value: 150, xAxis: 15),
-                                        DataPoint<double>(value: 75, xAxis: 20),
-                                        DataPoint<double>(value: 0, xAxis: 25),
-                                        DataPoint<double>(value: 5, xAxis: 30),
-                                        DataPoint<double>(value: 45, xAxis: 35),
-                                      ]
-                                    ),
-                                ],
-                                  config: BezierChartConfig(
-                                    snap: false,
-                                    backgroundColor: Colors.teal
-                                  ),
+                                  // bezierChartScale: BezierChartScale.CUSTOM,
+                                  //   xAxisCustomValues: const [0, 5, 10, 15, 20, 25, 30, 35],
+
+                                  series: [
+                                    BezierLine(data: [
+                                      DataPoint<double>(value: 10, xAxis: 0),
+                                      DataPoint<double>(value: 130, xAxis: 5),
+                                      DataPoint<double>(value: 50, xAxis: 10),
+                                      DataPoint<double>(value: 150, xAxis: 15),
+                                      DataPoint<double>(value: 75, xAxis: 20),
+                                      DataPoint<double>(value: 0, xAxis: 25),
+                                      DataPoint<double>(value: 5, xAxis: 30),
+                                      DataPoint<double>(value: 45, xAxis: 35),
+                                      DataPoint<double>(value: 5, xAxis: 40),
+                                      DataPoint<double>(value: 45, xAxis: 45),
+                                    ]),
+                                  ],
+                                  config: BezierChartConfig(snap: false, backgroundColor: Colors.teal),
+                                  bezierChartScale: BezierChartScale.MONTHLY,
                                 ),
                               ),
                               left: 16,
