ae0aa474dc17d1f0b69f4cbaa697943ee8184c57
Dreamwalker <aristojeff@gmail.com>
2021-04-20 20:10:41 +0900

    Update finance_card_home_page.dart

--- lib/ep762_finance_app/ui/finance_card_home_page.dart
+++ lib/ep762_finance_app/ui/finance_card_home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:line_icons/line_icons.dart';
 
 
 class FinanceCardPage extends StatefulWidget {
@@ -9,6 +11,60 @@ class FinanceCardPage extends StatefulWidget {
 class _FinanceCardPageState extends State<FinanceCardPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(LineIcons.bars),
+                        alignment: Alignment.centerLeft,
+                        padding: EdgeInsets.zero,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Services",
+                            style: GoogleFonts.montserrat(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.grey),
+                          ),
+
+                        ],
+                      ),
+                      // Spacer(),
+                      Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Stack(
+                            children: [
+                              Center(child: Icon(Icons.notifications_none)),
+                              Positioned(
+                                  right: 8,
+                                  top: 8,
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.red,
+                                    radius: 4,
+                                  ))
+                            ],
+                          ))
+                    ],
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
