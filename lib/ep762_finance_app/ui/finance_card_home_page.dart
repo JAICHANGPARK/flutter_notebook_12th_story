12e8ee433195c5a19d280d0433e379aab8d50df3
Dreamwalker <aristojeff@gmail.com>
2021-04-20 20:06:51 +0900

    Update finance_card_home_page.dart

--- lib/ep762_finance_app/ui/finance_card_home_page.dart
+++ lib/ep762_finance_app/ui/finance_card_home_page.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+
+
+class FinanceCardPage extends StatefulWidget {
+  @override
+  _FinanceCardPageState createState() => _FinanceCardPageState();
+}
+
+class _FinanceCardPageState extends State<FinanceCardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
