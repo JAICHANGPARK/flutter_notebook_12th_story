46b0365423edf4be6551e6eca6dfba6bd221db3a
Dreamwalker <aristojeff@gmail.com>
2021-04-21 20:38:24 +0900

    Create earbuds_controll_page.dart

---
+++ lib/ep766_earbuds_controller/ui/earbuds_controll_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class EarbudsControlPage extends StatefulWidget {
+  @override
+  _EarbudsControlPageState createState() => _EarbudsControlPageState();
+}
+
+class _EarbudsControlPageState extends State<EarbudsControlPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
