08747e8d5a58913e48f72a1ad58fb8a228e341ee
Dreamwalker <aristojeff@gmail.com>
2021-04-22 19:57:54 +0900

    Update earbuds_control_page.dart

--- lib/ep766_earbuds_controller/ui/earbuds_control_page.dart
+++ lib/ep766_earbuds_controller/ui/earbuds_control_page.dart
@@ -137,12 +137,15 @@ class _EarbudsControlPageState extends State<EarbudsControlPage> with SingleTick
                             fontWeight: FontWeight.bold,
                             fontSize: 20
                           ),),
-                          SizedBox(
-                            height: 160,
-                            child: Container(
-                              color: Colors.grey,
-                            ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                            child: SizedBox(
+                              height: 160,
+                              child: Container(
+                                color: Colors.grey,
+                              ),
 
+                            ),
                           )
                         ],
                       )
