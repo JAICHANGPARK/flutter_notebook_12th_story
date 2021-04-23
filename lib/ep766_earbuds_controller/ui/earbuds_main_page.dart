100b60d87820b279cb2615b7c3c2845cbcff8e5e
Dreamwalker <aristojeff@gmail.com>
2021-04-23 20:57:54 +0900

    Update earbuds_main_page.dart

--- lib/ep766_earbuds_controller/ui/earbuds_main_page.dart
+++ lib/ep766_earbuds_controller/ui/earbuds_main_page.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+
+
+class EarbudMainPage extends StatefulWidget {
+  @override
+  _EarbudMainPageState createState() => _EarbudMainPageState();
+}
+
+class _EarbudMainPageState extends State<EarbudMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
