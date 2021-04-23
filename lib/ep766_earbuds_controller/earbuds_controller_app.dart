67365947ed223f5c05f9327a61be3dea85666b24
Dreamwalker <aristojeff@gmail.com>
2021-04-23 20:59:56 +0900

    Update earbuds_controller_app.dart

--- lib/ep766_earbuds_controller/earbuds_controller_app.dart
+++ lib/ep766_earbuds_controller/earbuds_controller_app.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'ui/earbuds_control_page.dart';
+import 'ui/earbuds_main_page.dart';
 
 class EarBudsControllerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: EarbudsControlPage(),
+      home: EarbudMainPage(),
     );
   }
 }
