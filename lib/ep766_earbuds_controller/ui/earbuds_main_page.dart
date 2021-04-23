ca41f00971980cacd5c5e2fff2c9a5c94a64a5b6
Dreamwalker <aristojeff@gmail.com>
2021-04-23 21:07:45 +0900

    Update earbuds_main_page.dart

--- lib/ep766_earbuds_controller/ui/earbuds_main_page.dart
+++ lib/ep766_earbuds_controller/ui/earbuds_main_page.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
-
 class EarbudMainPage extends StatefulWidget {
   @override
   _EarbudMainPageState createState() => _EarbudMainPageState();
 }
 
-class _EarbudMainPageState extends State<EarbudMainPage> with SingleTickerProviderStateMixin{
+class _EarbudMainPageState extends State<EarbudMainPage> with SingleTickerProviderStateMixin {
   late TabController _tabController;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,30 +30,52 @@ class _EarbudMainPageState extends State<EarbudMainPage> with SingleTickerProvid
                   icon: Icon(Icons.power_settings_new),
                   color: Colors.white,
                 ),
-               Column(
-                 children: [
-                  Text("WF-XB700",style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),),
-                   Row(
-                     children: [
-                       CircleAvatar(radius: 3,backgroundColor: Colors.green,),
-                       SizedBox(width: 4,),
-                       Text("Connected to Dream's iPhone", style: TextStyle(
-                         fontSize: 12,
-                         color: Colors.grey
-                       ),),
-                     ],
-                   )
-                 ],
-               ),
+                Column(
+                  children: [
+                    Text(
+                      "WF-XB700",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 3,
+                          backgroundColor: Colors.green,
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text(
+                          "Connected to Dream's iPhone",
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
                 IconButton(
                   onPressed: () {},
                   icon: Icon(Icons.more_horiz),
                   color: Colors.white,
                 )
               ],
+            ),
+            SizedBox(
+              height: 280,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Colors.red,
+                  child: Column(
+                    children: [
+                      
+                    ],
+                  ),
+                ),
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 8),
