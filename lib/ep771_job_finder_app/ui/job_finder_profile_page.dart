cbb40eba1a859a81ad97755bba753988bc1a328c
Dreamwalker <aristojeff@gmail.com>
2021-04-26 19:56:44 +0900

    Create job_finder_profile_page.dart

---
+++ lib/ep771_job_finder_app/ui/job_finder_profile_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class JobFinderProfilePage extends StatefulWidget {
+  @override
+  _JobFinderProfilePageState createState() => _JobFinderProfilePageState();
+}
+
+class _JobFinderProfilePageState extends State<JobFinderProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
