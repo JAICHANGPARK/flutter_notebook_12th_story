afdbf88ebe5497c80f9a208b3d5d9e7c33a03a08
Dreamwalker <aristojeff@gmail.com>
2021-04-26 19:53:31 +0900

    Update job_finder_app.dart

--- lib/ep771_job_finder_app/job_finder_app.dart
+++ lib/ep771_job_finder_app/job_finder_app.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+
+class JobFinderApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: JobFinderProfilePage(),
+    );
+  }
+}
