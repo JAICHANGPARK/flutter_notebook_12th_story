caed2ed549ddbf6d563196af06fcebb4e43e93aa
Dreamwalker <aristojeff@gmail.com>
2021-04-26 20:01:29 +0900

    Update job_finder_profile_page.dart

--- lib/ep771_job_finder_app/ui/job_finder_profile_page.dart
+++ lib/ep771_job_finder_app/ui/job_finder_profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 
 
 class JobFinderProfilePage extends StatefulWidget {
@@ -15,6 +16,17 @@ class _JobFinderProfilePageState extends State<JobFinderProfilePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(onPressed: (){}, icon: Icon(LineIcons.arrowLeft)),
+                    Text("PROFILE"),
+                    IconButton(onPressed: (){}, icon: Icon(LineIcons.cog)),
+                  ],
+                ),
+              )
 
             ],
           ),
