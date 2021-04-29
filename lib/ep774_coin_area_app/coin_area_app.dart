396f198788041975daeca23205380524cf4be8f7
Dreamwalker <aristojeff@gmail.com>
2021-04-29 20:51:33 +0900

    Update coin_area_app.dart

--- lib/ep774_coin_area_app/coin_area_app.dart
+++ lib/ep774_coin_area_app/coin_area_app.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'ui/coin_area_home_page.dart';
+
 class CoinAreaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CoinAreaHomaApp(),
+      home: CoinAreaHomePage(),
     );
   }
 }
