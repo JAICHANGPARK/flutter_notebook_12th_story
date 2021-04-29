b0083cb5477676a95051c3cb1d252334c3e5a287
Dreamwalker <aristojeff@gmail.com>
2021-04-29 20:50:34 +0900

    Create coin_area_home_page.dart

---
+++ lib/ep774_coin_area_app/ui/coin_area_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CoinAreaHomePage extends StatefulWidget {
+  @override
+  _CoinAreaHomeAppState createState() => _CoinAreaHomeAppState();
+}
+
+class _CoinAreaHomeAppState extends State<CoinAreaHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
