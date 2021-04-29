203328e2dee8ec2faa8740d26d5bd46b5bfc0166
Dreamwalker <aristojeff@gmail.com>
2021-04-29 20:54:00 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'ep771_job_finder_app/job_finder_app.dart';
+import 'ep774_coin_area_app/coin_area_app.dart';
 
 void main() {
-  runApp(JobFinderApp());
+  runApp(CoinAreaApp());
 }
