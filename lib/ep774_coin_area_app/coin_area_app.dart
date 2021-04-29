e5132317f144a67e8784c1fd59440ba1cbda14cd
Dreamwalker <aristojeff@gmail.com>
2021-04-29 20:47:44 +0900

    Update coin_area_app.dart

--- lib/ep774_coin_area_app/coin_area_app.dart
+++ lib/ep774_coin_area_app/coin_area_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class  extends StatelessWidget {
+class CoinAreaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
