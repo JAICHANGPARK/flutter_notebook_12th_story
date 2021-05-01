eb5bb023a2d0fefec0ab7ea2752650f3602fe628
Dreamwalker <aristojeff@gmail.com>
2021-05-01 18:16:19 +0900

    Create coin_area_deposit_page.dart

---
+++ lib/ep774_coin_area_app/ui/coin_area_deposit_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CoinAreaDepositPage extends StatefulWidget {
+  @override
+  _CoinAreaDepositPageState createState() => _CoinAreaDepositPageState();
+}
+
+class _CoinAreaDepositPageState extends State<CoinAreaDepositPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
