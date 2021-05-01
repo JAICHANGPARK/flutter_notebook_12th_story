68a80562b9d67dde3cbfd8b27562b3b63678bc5a
Dreamwalker <aristojeff@gmail.com>
2021-05-01 18:19:51 +0900

    Update coin_area_deposit_page.dart

--- lib/ep774_coin_area_app/ui/coin_area_deposit_page.dart
+++ lib/ep774_coin_area_app/ui/coin_area_deposit_page.dart
@@ -9,11 +9,36 @@ class _CoinAreaDepositPageState extends State<CoinAreaDepositPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(child: Placeholder()),
-          Expanded(child: Placeholder()),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.menu,
+                        color: Colors.greenAccent,
+                      ),
+                      Spacer(),
+                      Column(
+                        children: [
+                          Text(
+                            "MAKE DEPOSIT",
+                            style: TextStyle(letterSpacing: 2, color: Colors.white, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      Spacer(),
+                      CircleAvatar(),
+                    ],
+                  ),
+                )),
+            Expanded(flex: 20, child: Placeholder()),
+          ],
+        ),
       ),
     );
   }
