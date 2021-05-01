e26cbb8a4a392e5be3186d9fc42110f148dd6f40
Dreamwalker <aristojeff@gmail.com>
2021-05-01 18:21:33 +0900

    Update coin_area_deposit_page.dart

--- lib/ep774_coin_area_app/ui/coin_area_deposit_page.dart
+++ lib/ep774_coin_area_app/ui/coin_area_deposit_page.dart
@@ -32,17 +32,27 @@ class _CoinAreaDepositPageState extends State<CoinAreaDepositPage> {
                             "MAKE DEPOSIT",
                             style: TextStyle(letterSpacing: 2, color: Colors.white, fontWeight: FontWeight.bold),
                           ),
-                          SizedBox(height: 8,),
+                          SizedBox(
+                            height: 8,
+                          ),
                           Row(
                             children: [
-                              Text("Step 1"),
+                              Text(
+                                "Step 1",
+                                style: TextStyle(
+                                  color: Colors.greenAccent,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                               Padding(
                                 padding: const EdgeInsets.all(8.0),
-                                child: Container(height: 2,width: 56,
-                                color: Colors.grey,),
+                                child: Container(
+                                  height: 2,
+                                  width: 56,
+                                  color: Colors.grey,
+                                ),
                               ),
                               Text("Step 2"),
-
                             ],
                           )
                         ],
