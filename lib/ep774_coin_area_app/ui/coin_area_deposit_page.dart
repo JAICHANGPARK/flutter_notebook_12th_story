89738a7744caacb484ac178447a488032ebedcda
Dreamwalker <aristojeff@gmail.com>
2021-05-02 19:33:20 +0900

    Update coin_area_deposit_page.dart

--- lib/ep774_coin_area_app/ui/coin_area_deposit_page.dart
+++ lib/ep774_coin_area_app/ui/coin_area_deposit_page.dart
@@ -130,6 +130,14 @@ class _CoinAreaDepositPageState extends State<CoinAreaDepositPage> {
                                 color: Colors.white.withOpacity(0.1),
                                 borderRadius: BorderRadius.circular(7),
                               ),
+                              child: Row(
+                                children: [
+                                  Text("1.23456789"),
+                                  Spacer(),
+                                  Text("BTC"),
+                                  Icon(Icons.arrow_drop_down_outlined, color: Colors.white,),
+                                ],
+                              ),
                             )
                           ],
                         ),
