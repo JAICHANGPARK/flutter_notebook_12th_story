12a43fa8c25b6fb4c0771653b0643293b8a8eefb
Dreamwalker <aristojeff@gmail.com>
2021-05-02 19:28:52 +0900

    Update coin_area_deposit_page.dart

--- lib/ep774_coin_area_app/ui/coin_area_deposit_page.dart
+++ lib/ep774_coin_area_app/ui/coin_area_deposit_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CoinAreaDepositPage extends StatefulWidget {
   @override
