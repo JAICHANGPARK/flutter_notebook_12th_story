abc4cd0ad8bc3d1bac3d5d402f782cd714851dd0
Dreamwalker <aristojeff@gmail.com>
2021-05-03 20:18:45 +0900

    Update screen_time_home_page.dart

--- lib/ep778_screen_time_app/ui/screen_time_home_page.dart
+++ lib/ep778_screen_time_app/ui/screen_time_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ScreenTimeHomePage extends StatefulWidget {
   @override
   _ScreenTimeHomePageState createState() => _ScreenTimeHomePageState();
@@ -11,17 +10,16 @@ class _ScreenTimeHomePageState extends State<ScreenTimeHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        elevation: 5,
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.grey[300], items: [
-          BottomNavigationBarItem(icon: Icon(Icons.bar_chart_outlined), label: "Activity"),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: "Family"),
-        BottomNavigationBarItem(icon: Icon(Icons.account_circle_outlined), label: "Friends"),
-        BottomNavigationBarItem(icon: Icon(Icons.bar_chart_outlined), label: "Coach"),
-        BottomNavigationBarItem(icon: Icon(Icons.settings_outlined), label: "Settings"),
-      ]
-        
-      ),
+          elevation: 5,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.grey[200],
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.bar_chart_outlined), label: "Activity"),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: "Family"),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle_outlined), label: "Friends"),
+            BottomNavigationBarItem(icon: Icon(Icons.bar_chart_outlined), label: "Coach"),
+            BottomNavigationBarItem(icon: Icon(Icons.settings_outlined), label: "Settings"),
+          ]),
     );
   }
 }
