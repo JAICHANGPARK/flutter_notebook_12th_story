b2ec822cc1548f6772990f7cd5eb69965532fd21
Dreamwalker <aristojeff@gmail.com>
2021-05-03 20:14:09 +0900

    Update screen_time_app.dart

--- lib/ep778_screen_time_app/screen_time_app.dart
+++ lib/ep778_screen_time_app/screen_time_app.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'ui/screen_time_home_page.dart';
+
 
 class ScreenTimeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      
+      initialRoute: "/",
+      routes: {
+        "/": (context) =>ScreenTimeHomePage()
+      },
     );
   }
 }
