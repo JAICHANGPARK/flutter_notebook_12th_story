f0568723bb59d45bcf55fe1e99beb439f9dfc37c
Dreamwalker <aristojeff@gmail.com>
2021-05-03 20:13:42 +0900

    Create screen_time_home_page.dart

---
+++ lib/ep778_screen_time_app/ui/screen_time_home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class ScreenTimeHomePage extends StatefulWidget {
+  @override
+  _ScreenTimeHomePageState createState() => _ScreenTimeHomePageState();
+}
+
+class _ScreenTimeHomePageState extends State<ScreenTimeHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
