1861425f000d720d01b98986d6cb695607a445fe
Dreamwalker <aristojeff@gmail.com>
2021-05-03 20:17:29 +0900

    Update screen_time_home_page.dart

--- lib/ep778_screen_time_app/ui/screen_time_home_page.dart
+++ lib/ep778_screen_time_app/ui/screen_time_home_page.dart
@@ -9,6 +9,17 @@ class ScreenTimeHomePage extends StatefulWidget {
 class _ScreenTimeHomePageState extends State<ScreenTimeHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey, items: [
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart_outlined), label: "Activity"),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: "Family"),
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle_outlined), label: "Friends"),
+        BottomNavigationBarItem(icon: Icon(Icons.bar_chart_outlined), label: "Activity"),
+        BottomNavigationBarItem(icon: Icon(Icons.bar_chart_outlined), label: "Activity"),
+      ],
+        
+      ),
+    );
   }
 }
