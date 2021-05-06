9ed886f2333e3f9af81c80044b988e77ed50d061
Dreamwalker <aristojeff@gmail.com>
2021-05-06 20:55:06 +0900

    Create crypto_home_page.dart

---
+++ lib/ep781_crypto_exchange_app/ui/crypto_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CryptoExchangeHomePage extends StatefulWidget {
+  const CryptoExchangeHomePage({Key? key}) : super(key: key);
+
+  @override
+  _CryptoExchangeHomePageState createState() => _CryptoExchangeHomePageState();
+}
+
+class _CryptoExchangeHomePageState extends State<CryptoExchangeHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
