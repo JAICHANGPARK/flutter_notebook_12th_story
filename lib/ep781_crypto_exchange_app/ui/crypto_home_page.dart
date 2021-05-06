88430d1facf2b7f1d7c94c3c2254be40279b511f
Dreamwalker <aristojeff@gmail.com>
2021-05-06 20:59:27 +0900

    Update crypto_home_page.dart

--- lib/ep781_crypto_exchange_app/ui/crypto_home_page.dart
+++ lib/ep781_crypto_exchange_app/ui/crypto_home_page.dart
@@ -10,6 +10,24 @@ class CryptoExchangeHomePage extends StatefulWidget {
 class _CryptoExchangeHomePageState extends State<CryptoExchangeHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: Placeholder()),
+          Expanded(child: Placeholder()),
+          Expanded(child: Placeholder()),
+
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.dashboard_outlined), label: "Dashboard"),
+          BottomNavigationBarItem(icon: Icon(Icons.wallet_giftcard), label: "Wallet"),
+          BottomNavigationBarItem(icon: Icon(Icons.chat_bubble_outline), label: "Chat"),
+          BottomNavigationBarItem(icon: Icon(Icons.add_shopping_cart_outlined), label: "Markets"),
+        ],
+      ),
+    );
   }
 }
