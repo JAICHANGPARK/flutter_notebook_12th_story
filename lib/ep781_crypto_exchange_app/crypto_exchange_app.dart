2b1585863bc8e36ac69258177418774cde77ea9f
Dreamwalker <aristojeff@gmail.com>
2021-05-06 20:55:25 +0900

    Update crypto_exchange_app.dart

--- lib/ep781_crypto_exchange_app/crypto_exchange_app.dart
+++ lib/ep781_crypto_exchange_app/crypto_exchange_app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'ui/crypto_home_page.dart';
+
 class CryptoExchangeApp extends StatelessWidget {
   const CryptoExchangeApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: CryptoExchangeHomePage(),
     );
   }
 }
