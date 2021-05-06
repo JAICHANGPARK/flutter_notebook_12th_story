748f1c97e49cc5527598e6d85c32dd0e81020993
Dreamwalker <aristojeff@gmail.com>
2021-05-06 21:06:42 +0900

    Update crypto_home_page.dart

--- lib/ep781_crypto_exchange_app/ui/crypto_home_page.dart
+++ lib/ep781_crypto_exchange_app/ui/crypto_home_page.dart
@@ -8,12 +8,14 @@ class CryptoExchangeHomePage extends StatefulWidget {
 }
 
 class _CryptoExchangeHomePageState extends State<CryptoExchangeHomePage> {
-  // 71 0 249 blue
+  // 71 0 249 blue 47 00 f9
   // 119 254 114 green  77 fE 72
   // 243 247 253 background f4 f7 fd
-
+  int _selectedTab = 0;
   Color _cryptoAppGreenColor = Color(0xff77fe72);
-  Color _cryptoAppBackgroundColor= Color(0xfff4f7fd);
+  Color _cryptoAppBlueColor = Color(0xff4700f0);
+  Color _cryptoAppBackgroundColor = Color(0xfff4f7fd);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,23 +24,40 @@ class _CryptoExchangeHomePageState extends State<CryptoExchangeHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(child: Container(
-              color: Colors.white,
-            ), flex: 3,),
-            Expanded(child: Placeholder(), flex: 22,),
-            Expanded(child: Row(
-              children: [
-                Expanded(child: Placeholder()),
-                Expanded(child: Container(
-                  color: _cryptoAppGreenColor,
-                )),
-              ],
-            ),flex: 2,),
-
+            Expanded(
+              child: Container(
+                color: Colors.white,
+              ),
+              flex: 3,
+            ),
+            Expanded(
+              child: Placeholder(),
+              flex: 22,
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(child: Container(
+                    color: _cryptoAppBlueColor,
+                  )),
+                  Expanded(
+                      child: Container(
+                    color: _cryptoAppGreenColor,
+                  )),
+                ],
+              ),
+              flex: 2,
+            ),
           ],
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        onTap: (idx){
+          setState(() {
+            _selectedTab = idx;
+          });
+        },
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.dashboard_outlined), label: "Dashboard"),
