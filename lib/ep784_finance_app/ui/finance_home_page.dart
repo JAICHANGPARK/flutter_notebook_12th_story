4b6817939bd02c2545f4a1b1066a887b26c0687d
Dreamwalker <aristojeff@gmail.com>
2021-05-09 18:05:45 +0900

    Update finance_home_page.dart

---
+++ lib/ep784_finance_app/ui/finance_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FinanceHomePage extends StatefulWidget {
+  const FinanceHomePage({Key? key}) : super(key: key);
+
+  @override
+  _FinanceHomePageState createState() => _FinanceHomePageState();
+}
+
+class _FinanceHomePageState extends State<FinanceHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
