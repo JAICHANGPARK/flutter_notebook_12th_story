3380ccc282ca63282436e3eb4c2c906f5cd267d7
Dreamwalker <aristojeff@gmail.com>
2021-05-09 18:11:09 +0900

    Update finance_home_page.dart

--- lib/ep784_finance_app/ui/finance_home_page.dart
+++ lib/ep784_finance_app/ui/finance_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FinanceHomePage extends StatefulWidget {
   const FinanceHomePage({Key? key}) : super(key: key);
 
@@ -11,23 +10,38 @@ class FinanceHomePage extends StatefulWidget {
 class _FinanceHomePageState extends State<FinanceHomePage> {
   int _bottomTabIdx = 0;
   late PageController _pageController;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _pageController = PageController();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-
+        controller: _pageController,
         children: [
           SingleChildScrollView(
             child: Column(
-              children: [
-
-              ],
+              children: [],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [],
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [],
             ),
           ),
         ],
@@ -35,7 +49,7 @@ class _FinanceHomePageState extends State<FinanceHomePage> {
       bottomNavigationBar: BottomNavigationBar(
         elevation: 3,
         currentIndex: _bottomTabIdx,
-        onTap: (idx){
+        onTap: (idx) {
           setState(() {
             _bottomTabIdx = idx;
           });
@@ -44,13 +58,11 @@ class _FinanceHomePageState extends State<FinanceHomePage> {
         showSelectedLabels: false,
         showUnselectedLabels: false,
         unselectedItemColor: Colors.grey,
-
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.account_balance_wallet), label: "Wallet"),
           BottomNavigationBarItem(icon: Icon(Icons.insert_chart), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Home"),
-
         ],
       ),
     );
