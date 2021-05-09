045e4e17cb4aa86280297b7fd4c668b80a988a81
Dreamwalker <aristojeff@gmail.com>
2021-05-09 18:06:11 +0900

    Update finance_chart_page.dart

---
+++ lib/ep784_finance_app/ui/finance_chart_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FinanceChartPage extends StatefulWidget {
+  const FinanceChartPage({Key? key}) : super(key: key);
+
+  @override
+  _FinanceChartPageState createState() => _FinanceChartPageState();
+}
+
+class _FinanceChartPageState extends State<FinanceChartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
