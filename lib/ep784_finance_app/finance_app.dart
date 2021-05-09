e1784255187bb1b646afa1206f45f95d29019cad
Dreamwalker <aristojeff@gmail.com>
2021-05-09 18:04:51 +0900

    Update finance_app.dart

--- lib/ep784_finance_app/finance_app.dart
+++ lib/ep784_finance_app/finance_app.dart
@@ -6,6 +6,8 @@ class FinanceApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: FInanceChartPage(),
+    );
   }
 }
