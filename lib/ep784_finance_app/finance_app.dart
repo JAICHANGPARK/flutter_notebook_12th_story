c3edbadc97c7ad6a6613983b57ea09ee5fc08215
Dreamwalker <aristojeff@gmail.com>
2021-05-09 18:04:31 +0900

    Update finance_app.dart

---
+++ lib/ep784_finance_app/finance_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FinanceApp extends StatelessWidget {
+  const FinanceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
