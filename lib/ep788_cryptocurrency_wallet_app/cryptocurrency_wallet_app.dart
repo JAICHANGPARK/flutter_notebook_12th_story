b78e492052e45c967d25a25f019c67342cf9e41b
Dreamwalker <aristojeff@gmail.com>
2021-05-13 20:56:53 +0900

    Update cryptocurrency_wallet_app.dart

--- lib/ep788_cryptocurrency_wallet_app/cryptocurrency_wallet_app.dart
+++ lib/ep788_cryptocurrency_wallet_app/cryptocurrency_wallet_app.dart
@@ -11,47 +11,4 @@ class CryptocurrencyWalletApp extends StatelessWidget {
   }
 }
 
-class CryptocurrencyWalletHomePage extends StatefulWidget {
-  const CryptocurrencyWalletHomePage({Key? key}) : super(key: key);
-
-  @override
-  _CryptocurrencyWalletHomePageState createState() => _CryptocurrencyWalletHomePageState();
-}
-
-class _CryptocurrencyWalletHomePageState extends State<CryptocurrencyWalletHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.account_balance_wallet,
-              ),
-              label: "Wallet"),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.account_balance_wallet,
-              ),
-              label: "Portfolio"),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.account_balance_wallet,
-              ),
-              label: "News"),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.account_balance_wallet,
-              ),
-              label: "Settings"),
-        ],
-      ),
-    );
-  }
-}
-
 
