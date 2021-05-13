2c20b49f450d205ad619b5f93c93955d4ced26d2
Dreamwalker <aristojeff@gmail.com>
2021-05-13 20:54:52 +0900

    Update cryptocurrency_wallet_app.dart

--- lib/ep788_cryptocurrency_wallet_app/cryptocurrency_wallet_app.dart
+++ lib/ep788_cryptocurrency_wallet_app/cryptocurrency_wallet_app.dart
@@ -37,3 +37,18 @@ class CryptocurrencyWalletApp extends StatelessWidget {
     );
   }
 }
+
+class  extends StatefulWidget {
+  const ({Key key}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
