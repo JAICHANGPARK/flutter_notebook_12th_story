5b1f68c2b0b2d488577c0288291f6a2288ea21ac
Dreamwalker <aristojeff@gmail.com>
2021-05-15 17:44:16 +0900

    Create cryptocurrency_wallet_news_page.dart

---
+++ lib/ep788_cryptocurrency_wallet_app/ui/cryptocurrency_wallet_news_page.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+
+import 'package:badges/badges.dart';
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
+import 'package:chart_sparkline/flutter_sparkline.dart';
+import 'package:flutter/material.dart';
+
+class CryptocurrencyWalletNewsPage extends StatefulWidget {
+  const CryptocurrencyWalletNewsPage({Key? key}) : super(key: key);
+
+  @override
+  _CryptocurrencyWalletNewsPageState createState() => _CryptocurrencyWalletNewsPageState();
+}
+
+class _CryptocurrencyWalletNewsPageState extends State<CryptocurrencyWalletNewsPage>
+    with SingleTickerProviderStateMixin {
+  int _bottomIndex = 0;
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 6, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: (idx) {
+          setState(() {
+            _bottomIndex = idx;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_balance_wallet,
+              ),
+              label: "Wallet"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.pie_chart,
+              ),
+              label: "Portfolio"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.list_alt,
+              ),
+              label: "News"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_circle,
+              ),
+              label: "Settings"),
+        ],
+      ),
+    );
+  }
+}
