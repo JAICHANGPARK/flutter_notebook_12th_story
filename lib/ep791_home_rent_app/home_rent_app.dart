43dd5c05f3779f731fb0d2d2df544e6cf8a4775a
Dreamwalker <aristojeff@gmail.com>
2021-05-16 17:38:43 +0900

    Create home_rent_app.dart

---
+++ lib/ep791_home_rent_app/home_rent_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeRentApp extends StatelessWidget {
+  const HomeRentApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
