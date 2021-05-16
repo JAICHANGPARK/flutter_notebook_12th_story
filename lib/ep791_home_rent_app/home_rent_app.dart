ee95da2d16febb4e751c251ab57b14261ce78ab3
Dreamwalker <aristojeff@gmail.com>
2021-05-16 17:40:31 +0900

    Update home_rent_app.dart

--- lib/ep791_home_rent_app/home_rent_app.dart
+++ lib/ep791_home_rent_app/home_rent_app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_12th_story/ep791_home_rent_app/ui/home_rent_home_page.dart';
 
 class HomeRentApp extends StatelessWidget {
   const HomeRentApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomeRentHomePage(),
+    );
   }
 }
