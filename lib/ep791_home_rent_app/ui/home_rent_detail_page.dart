46dd7ace46bb897d07e4d4ddc03ef8487f0f216c
Dreamwalker <aristojeff@gmail.com>
2021-05-19 16:59:08 +0900

    Update home_rent_detail_page.dart

--- lib/ep791_home_rent_app/ui/home_rent_detail_page.dart
+++ lib/ep791_home_rent_app/ui/home_rent_detail_page.dart
@@ -11,56 +11,59 @@ class _HomeRentDetailPageState extends State<HomeRentDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 84,
-          padding: EdgeInsets.all(16),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Price",
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Text.rich(TextSpan(children: [
-                    TextSpan(
-                        text: "\$820",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        )),
-                    TextSpan(
-                        text: "/Month",
-                        style: TextStyle(
-                          fontSize: 12,
-                        )),
-                  ]))
-                ],
-              ),
-              Spacer(),
-              Container(
-                height: 64,
-                width: 120,
-                decoration: BoxDecoration(
+      bottomNavigationBar: buildBottomAppBarWidget(),
+    );
+  }
+  Widget buildBottomAppBarWidget(){
+    return BottomAppBar(
+      child: Container(
+        height: 84,
+        padding: EdgeInsets.all(16),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Price",
+                  style: TextStyle(fontSize: 12, color: Colors.grey),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text.rich(TextSpan(children: [
+                  TextSpan(
+                      text: "\$820",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  TextSpan(
+                      text: "/Month",
+                      style: TextStyle(
+                        fontSize: 12,
+                      )),
+                ]))
+              ],
+            ),
+            Spacer(),
+            Container(
+              height: 64,
+              width: 120,
+              decoration: BoxDecoration(
                   color: Colors.black,
                   borderRadius: BorderRadius.circular(16)
+              ),
+              child: Center(
+                child: Text(
+                  "Rent Now",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
                 ),
-                child: Center(
-                  child: Text(
-                    "Rent Now",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
-                  ),
-                ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
