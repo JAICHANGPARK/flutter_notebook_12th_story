2323ce3bef0a34da85a8c8d93028798de23a3ee7
Dreamwalker <aristojeff@gmail.com>
2021-05-19 16:53:03 +0900

    Update home_rent_detail_page.dart

--- lib/ep791_home_rent_app/ui/home_rent_detail_page.dart
+++ lib/ep791_home_rent_app/ui/home_rent_detail_page.dart
@@ -13,9 +13,26 @@ class _HomeRentDetailPageState extends State<HomeRentDetailPage> {
     return Scaffold(
       bottomNavigationBar: BottomAppBar(
         child: Container(
+          height: 84,
           child: Row(
             children: [
-              
+                Column(
+                  children: [
+                    Text("Price"),
+                    Text.rich(TextSpan(children:
+                    [
+                      TextSpan(
+                        text: "\$820"
+                      ),
+                      TextSpan(
+                          text: "/Month",
+                        style: TextStyle(
+                          fontSize: 12,
+                        )
+                      ),
+                    ]))
+                  ],
+                )
             ],
           ),
 
