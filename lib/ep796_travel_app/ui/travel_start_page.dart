8ad54cb492fc0c303fd075bc002252772bbbd1b0
Dreamwalker <aristojeff@gmail.com>
2021-05-21 21:42:12 +0900

    Update travel_start_page.dart

--- lib/ep796_travel_app/ui/travel_start_page.dart
+++ lib/ep796_travel_app/ui/travel_start_page.dart
@@ -16,16 +16,19 @@ class _TravelStartPageState extends State<TravelStartPage> {
       body: SafeArea(
         child: Stack(
           children: [
-            Positioned(child: KenBurns(child: Image.network("https://cdn.pixabay.com/photo/2014/08/12/00/01/santorini-416135_960_720.jpg",
+            Positioned(child: KenBurns(
+              maxScale: 1.5,
+
+              child: Image.network("https://cdn.pixabay.com/photo/2014/08/12/00/01/santorini-416135_960_720.jpg",
             ),), left: 0, right: 0,top: 0, bottom: 0,),
-            Positioned(
-                child: Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image:
-                          NetworkImage("https://cdn.pixabay.com/photo/2014/08/12/00/01/santorini-416135_960_720.jpg"),
-                      fit: BoxFit.cover)),
-            )),
+            // Positioned(
+            //     child: Container(
+            //   decoration: BoxDecoration(
+            //       image: DecorationImage(
+            //           image:
+            //               NetworkImage("https://cdn.pixabay.com/photo/2014/08/12/00/01/santorini-416135_960_720.jpg"),
+            //           fit: BoxFit.cover)),
+            // )),
             Positioned(
               child: Text(
                 "T R A V E L",
