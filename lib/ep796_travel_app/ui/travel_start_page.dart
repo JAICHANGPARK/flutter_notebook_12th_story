739a70d3da72ef6230094ebe0bd49166de266933
Dreamwalker <aristojeff@gmail.com>
2021-05-21 21:29:48 +0900

    Update travel_start_page.dart

--- lib/ep796_travel_app/ui/travel_start_page.dart
+++ lib/ep796_travel_app/ui/travel_start_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TravelStartPage extends StatefulWidget {
   const TravelStartPage({Key? key}) : super(key: key);
 
@@ -15,19 +14,23 @@ class _TravelStartPageState extends State<TravelStartPage> {
       body: SafeArea(
         child: Stack(
           children: [
-            Positioned(child: Container(
+            Positioned(
+                child: Container(
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage("https://cdn.pixabay.com/photo/2014/08/12/00/01/santorini-416135_960_720.jpg"),
-                  fit: BoxFit.cover
-                )
-              ),
+                  image: DecorationImage(
+                      image:
+                          NetworkImage("https://cdn.pixabay.com/photo/2014/08/12/00/01/santorini-416135_960_720.jpg"),
+                      fit: BoxFit.cover)),
             )),
-            Positioned(child: Text("T R A V E L", style: TextStyle(
-              fontSize: 24
-
-            ),), left: 24, top: 48,)
-
+            Positioned(
+              child: Text(
+                "T R A V E L",
+                style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+              left: 24,
+              top: 48,
+            ),
+            Positioned(child: Container(), left: 0, right: 0, bottom: 0,)
           ],
         ),
       ),
