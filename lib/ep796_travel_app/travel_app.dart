65706bfa40d0a629238c3450dde5380052f105c7
Dreamwalker <aristojeff@gmail.com>
2021-05-21 21:25:25 +0900

    Update travel_app.dart

--- lib/ep796_travel_app/travel_app.dart
+++ lib/ep796_travel_app/travel_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_12th_story/ep796_travel_app/ui/travel_start_page.dart';
 
 class TravelApp extends StatelessWidget {
   const TravelApp({Key? key}) : super(key: key);
@@ -6,7 +7,9 @@ class TravelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      routes: {
+        "/" : (context) => TravelStartPage(),
+      },
     );
   }
 }
