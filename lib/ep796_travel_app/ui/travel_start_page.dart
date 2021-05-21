b1496c8ca6b38ff5a0461d836202c1c63b6cc493
Dreamwalker <aristojeff@gmail.com>
2021-05-21 21:27:07 +0900

    Update travel_start_page.dart

--- lib/ep796_travel_app/ui/travel_start_page.dart
+++ lib/ep796_travel_app/ui/travel_start_page.dart
@@ -14,7 +14,14 @@ class _TravelStartPageState extends State<TravelStartPage> {
     return Scaffold(
       body: Stack(
         children: [
-
+          Positioned(child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage("https://cdn.pixabay.com/photo/2014/08/12/00/01/santorini-416135_960_720.jpg"),
+                fit: BoxFit.cover
+              )
+            ),
+          ))
         ],
       ),
     );
