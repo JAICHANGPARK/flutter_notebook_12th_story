8cb0583015b9a5b9524e4378a9ed60f50123eb64
Dreamwalker <aristojeff@gmail.com>
2021-05-21 21:36:44 +0900

    Update travel_start_page.dart

--- lib/ep796_travel_app/ui/travel_start_page.dart
+++ lib/ep796_travel_app/ui/travel_start_page.dart
@@ -44,7 +44,9 @@ class _TravelStartPageState extends State<TravelStartPage> {
                       "Make some memories",
                       style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(height: 8,),
+                    SizedBox(
+                      height: 8,
+                    ),
                     Padding(
                       padding: const EdgeInsets.only(right: 64),
                       child: Text(
@@ -54,22 +56,33 @@ class _TravelStartPageState extends State<TravelStartPage> {
                         ),
                       ),
                     ),
-                    SizedBox(height: 24,),
+                    SizedBox(
+                      height: 24,
+                    ),
                     Row(
                       children: [
                         Spacer(),
                         PhysicalModel(
                           color: Colors.white,
                           elevation: 4,
-                          shadowColor:Colors.blue[200]! ,
+                          shadowColor: Colors.blue[400]!,
+                          borderRadius: BorderRadius.circular(24),
                           child: Container(
                             height: 42,
                             width: 160,
-                            decoration: BoxDecoration(
-                              color: Colors.blue,
+                            decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(24)),
+                            
+                            child: Row(
+                              children: [
+                                Text("Start"),
+                                Icon(
+                                  Icons.arrow_forward,
+                                  color: Colors.white,
+                                )
+                              ],
                             ),
                           ),
-                        )
+                        ),
                       ],
                     )
                   ],
