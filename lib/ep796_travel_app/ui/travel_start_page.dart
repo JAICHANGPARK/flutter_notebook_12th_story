b369d9c6b07da1bde7ef7bf245c28492b9d778c2
Dreamwalker <aristojeff@gmail.com>
2021-05-21 21:25:04 +0900

    Update travel_start_page.dart

--- lib/ep796_travel_app/ui/travel_start_page.dart
+++ lib/ep796_travel_app/ui/travel_start_page.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+
+
+class TravelStartPage extends StatefulWidget {
+  const TravelStartPage({Key? key}) : super(key: key);
+
+  @override
+  _TravelStartPageState createState() => _TravelStartPageState();
+}
+
+class _TravelStartPageState extends State<TravelStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
