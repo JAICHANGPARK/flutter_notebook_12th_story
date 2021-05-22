51f8228249b1bd5ac8529d06de8654d414e3810f
Dreamwalker <aristojeff@gmail.com>
2021-05-22 19:35:33 +0900

    Update travel_home_page.dart

--- lib/ep796_travel_app/ui/travel_home_page.dart
+++ lib/ep796_travel_app/ui/travel_home_page.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-
 
 class TravelHomePage extends StatefulWidget {
   const TravelHomePage({Key? key}) : super(key: key);
@@ -11,91 +9,130 @@ class TravelHomePage extends StatefulWidget {
 
 class _TravelHomePageState extends State<TravelHomePage> {
   num _tabIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            
-          ],
-        ),
-      ),
-    ),
-    bottomNavigationBar: ClipRRect(
-      borderRadius: BorderRadius.only(
-        topRight: Radius.circular(24),
-        topLeft: Radius.circular(24),
-      ),
-
-      child: BottomAppBar(
-        elevation: 24,
-        color: Colors.white,
-        child: Container(
-          height: 82,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-              GestureDetector(
-                onTap: (){
-                  setState(() {
-                    _tabIndex = 0;
-                  });
-                },
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                  Icon(Icons.search, size: 32, color: _tabIndex == 0 ? Colors.blue : Colors.black,),
-                  SizedBox(height: 8,),
-                  Text("Discover",style: TextStyle(
-                    color: _tabIndex == 0 ? Colors.blue : Colors.black,
-                  ),)
-                ],),
-              ),
-              GestureDetector(
-                onTap: (){
-                  setState(() {
-                    _tabIndex = 1;
-                  });
-                },
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.shopping_bag_outlined, size: 32,color: _tabIndex == 1 ? Colors.blue : Colors.black,),
-                    SizedBox(height: 8,),
-                    Text("Bookings",style: TextStyle(
-                      color: _tabIndex == 1 ? Colors.blue : Colors.black,
-                    ),)
-                  ],),
-              ),
-              GestureDetector(
-                onTap: (){
-                  setState(() {
-                    _tabIndex = 2;
-                  });
-                },
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 16,
-                    ),
-                    SizedBox(height: 8,),
-                    Text("Profile",style: TextStyle(
-                      color: _tabIndex == 2 ? Colors.blue : Colors.black,
-                    ),)
-                  ],),
+              Row(
+                children: [
+                  Column(
+                    children: [Text("Hi Dreamwalker!"), Text("Let's plan a journey!")],
+                  ),
+                  Spacer(),
+                  FloatingActionButton(
+                    heroTag: "top_fab",
+                    onPressed: () {},
+                    mini: true,
+                    backgroundColor: Colors.white,
+                  )
+                ],
               )
             ],
           ),
         ),
       ),
-    ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(24),
+          topLeft: Radius.circular(24),
+        ),
+        child: BottomAppBar(
+          elevation: 24,
+          color: Colors.white,
+          child: Container(
+            height: 82,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _tabIndex = 0;
+                    });
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.search,
+                        size: 32,
+                        color: _tabIndex == 0 ? Colors.blue : Colors.black,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Discover",
+                        style: TextStyle(
+                          color: _tabIndex == 0 ? Colors.blue : Colors.black,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _tabIndex = 1;
+                    });
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.shopping_bag_outlined,
+                        size: 32,
+                        color: _tabIndex == 1 ? Colors.blue : Colors.black,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Bookings",
+                        style: TextStyle(
+                          color: _tabIndex == 1 ? Colors.blue : Colors.black,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _tabIndex = 2;
+                    });
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 16,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Profile",
+                        style: TextStyle(
+                          color: _tabIndex == 2 ? Colors.blue : Colors.black,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
