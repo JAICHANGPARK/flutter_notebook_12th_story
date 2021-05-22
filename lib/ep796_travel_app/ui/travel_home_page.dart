61605c1cf2f766e42351400019abce330a5e195d
Dreamwalker <aristojeff@gmail.com>
2021-05-22 19:25:23 +0900

    Update travel_home_page.dart

--- lib/ep796_travel_app/ui/travel_home_page.dart
+++ lib/ep796_travel_app/ui/travel_home_page.dart
@@ -13,6 +13,7 @@ class _TravelHomePageState extends State<TravelHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
     bottomNavigationBar: ClipRRect(
       borderRadius: BorderRadius.only(
         topRight: Radius.circular(24),
