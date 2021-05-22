52e703893d45d43c78cc2e25cc41f05dcd186457
Dreamwalker <aristojeff@gmail.com>
2021-05-22 19:22:12 +0900

    Update travel_home_page.dart

--- lib/ep796_travel_app/ui/travel_home_page.dart
+++ lib/ep796_travel_app/ui/travel_home_page.dart
@@ -1,1 +1,19 @@
 
+import 'package:flutter/material.dart';
+
+
+class TravelHomePage extends StatefulWidget {
+  const TravelHomePage({Key? key}) : super(key: key);
+
+  @override
+  _TravelHomePageState createState() => _TravelHomePageState();
+}
+
+class _TravelHomePageState extends State<TravelHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
